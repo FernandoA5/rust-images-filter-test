abfb861d50c7bb1db8653c5918c6b998c3fb7efe
FernandoA5 <fernandoalcalacasas@hotmail.com>
2024-01-03 04:40:35 -0600

    Agregado procesamiento multi-hilo con rayon

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
-use std::iter;
-
-use image::{GenericImageView, ImageBuffer, DynamicImage};
+use rayon::prelude::*;
+use image::{GenericImageView, ImageBuffer};
 
 fn main() {
+
+    //INICIAMOS CONTADOR DE TIEMPO
+    let now = std::time::Instant::now();
+
+
     let img = image::open("src/test.png");
     let img_clone = img.unwrap().clone();
 
@@ -14,12 +18,27 @@ fn main() {
 
     let image_bytes = img_clone.into_bytes();
 
-    let mut new_image_pixels: Vec<u8> = Vec::new();
-    for i in (0..image_bytes.len()).step_by(3){
-        let suma: i32 = image_bytes[i..i+3].iter().map(|&x| x as i32).sum();
-        let promedio: u8 = (suma / 3) as u8;
-        new_image_pixels.push(promedio as u8);
-    }
+    // let mut new_image_pixels: Vec<u8> = Vec::new();
+    // for i in (0..image_bytes.len()).step_by(3){
+    //     let suma: i32 = image_bytes[i..i+3].iter().map(|&x| x as i32).sum();
+    //     let promedio: u8 = (suma / 3) as u8;
+    //     new_image_pixels.push(promedio as u8);
+    // }
+
+    let num_threads = rayon::current_num_threads();
+    println!("Número de hilos: {}", num_threads);
+
+
+    let new_image_pixels: Vec<u8> = image_bytes.par_chunks(3)
+        .map(|chunk| {
+            let suma: i32 = chunk.iter().map(|&x| x as i32).sum();
+            let promedio: u8 = (suma / 3) as u8;
+            promedio as u8
+        }).collect();
+
+
+
+
     //GUARDAR LA IMAGEN new_image_pixels
     let mut new_image: ImageBuffer<image::Rgb<u8>, Vec<u8>> = ImageBuffer::new(width, height);
     for (x, y, pixel) in new_image.enumerate_pixels_mut() {
@@ -27,6 +46,10 @@ fn main() {
         *pixel = image::Rgb([new_image_pixels[index], new_image_pixels[index], new_image_pixels[index]]);
     }
     new_image.save("src/out_test.png").unwrap();
+
+    //FINALIZAMOS CONTADOR DE TIEMPO
+    let elapsed = now.elapsed();
+    println!("Tiempo de ejecucion: {:?}", elapsed);
 
 }
 
